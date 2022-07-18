ace86354034dccb182c6eca923e7e5930104ff0d
CarlosPrieto <carlosprieto66@icloud.com>
2022-07-18 11:39:31 -0700

    assigned playable choice to randumNum generated by the computer.

--- script.js
+++ script.js
@@ -6,6 +6,13 @@
 const getComputerChoice = () => {
     let randomNum = Math.floor(Math.random() * 3);
     
+    if (randomNum === 0) {
+         return 'rock';
+        } else if (randomNum === 1) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        };
 };
 
 console.log(getComputerChoice());
