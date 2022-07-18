960b4cb2694f9951b160c23edc9487e9cce31254
CarlosPrieto <carlosprieto66@icloud.com>
2022-07-18 11:34:46 -0700

    function getComputerChoice picks a random number

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Creating a funtion Where the computer will pick a random number between 0-2. 
+// And then assign one of the playable choices to the number.
+
+const getComputerChoice = () => {
+    let randomNum = Math.floor(Math.random() * 3);
+    
+};
+
+console.log(getComputerChoice());
